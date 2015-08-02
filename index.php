e86a9a1dd4bf7d91433813e031fa40b75ea4432d
hehanlin <china.hehahnlin@gmail.com>
2015-08-02 11:10:42 +0800

    判断url

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
  * Time: 10:41
  */
 
-//定义工程根目录
+
 define('BASEDIR',__DIR__);
-//引入自动加载类文件
-require_once BASEDIR.'/System/Loader.php';
-//调用spl库自动加载函数
+
+require_once BASEDIR . '/System/Loader.php';
+
 spl_autoload_register('\\System\\Loader::autoload');
-//单例application
+
 System\Application::getInstance(BASEDIR)->dispatch();
